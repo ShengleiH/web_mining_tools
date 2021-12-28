127a87fb22d1eee757b75818be140d9e0aecb390
huangshenglei <huangshenglei@meituan.com>
2021-12-28 14:34:48 +0800

    update parsing

--- xinyang_baike_mining.py
+++ xinyang_baike_mining.py
@@ -11,6 +11,7 @@ sys.path.append(father_path)
 import time
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 
 
 def generate_seed_url():
@@ -146,6 +147,111 @@ def parsing_item_html(data_dir, out_path):
     processed_results_df.to_excel(out_path, index=False)
 
 
+def parsing_device_html(data_dir, out_path, mode):
+    seed_url_pattern_dict = generate_seed_url()
+    item_urls = seed_url_pattern_dict[mode]
+
+    processed_results = []
+    debug_cnt = 0
+    for url in tqdm(item_urls):
+        file_path = os.path.join(data_dir, url.split('/')[-1]) + ".html"
+        if not os.path.exists(file_path):
+            processed_results.append([url])
+            continue
+        lines = open(file_path, 'r', encoding='utf-8').readlines()
+        soup = BeautifulSoup(''.join(lines), "html.parser")
+        box_info = soup.findAll(name="div", attrs={"class": "box product"})
+        box_info = box_info[0] if len(box_info) == 1 else None
+        if box_info is None:
+            processed_results.append([url])
+            continue
+
+        # 1. 项目名，别称，简介
+        try:
+            introduction = box_info.findAll(name="div", attrs={"class": "item-pannel introduce"})[0]
+            item_name = introduction.findAll(name="h1", attrs={"class": "name"})[0].text.strip()
+        except:
+            # 不允许连项目名都没有
+            processed_results.append([url])
+            continue
+
+        try:
+            item_aliases = introduction.findAll(name="span", attrs={"class": "alias"})[0].text.strip().replace("别名：", "").split("、")
+        except:
+            item_aliases = []
+
+        try:
+            item_desc = introduction.findAll(name="p", attrs={"class": "desc"})[0].text.strip()
+        except:
+            item_desc = ""
+
+        # 2. 功效
+        try:
+            summary_box = box_info.findAll(name="section", attrs={"class": "fixed-list", "id": "summary"})[0]
+        except:
+            summary_box = None
+
+        try:
+            effects_div = summary_box.findAll(name="div", attrs={"class": "labels"})[0]
+            effects = [s.text.strip() for s in effects_div.findAll(name="span", attrs={"class": "label"})]
+        except:
+            effects = []
+
+        # 3. 适用项目
+        try:
+            item_div = summary_box.findAll(name="div", attrs={"id": "item"})[0]
+            items = [s.text.strip() for s in item_div.findAll(name="span", attrs={"class": "label"})]
+        except:
+            items = []
+
+        # 4. 仪器档案
+        try:
+            archive_dict = defaultdict(str)
+            archive_div = box_info.findAll(name="div", attrs={'class': 'archives'})[0]
+            right_rows = archive_div.findAll(name='div', attrs={"class": 'r'})
+            left_rows = archive_div.findAll(name='div', attrs={"class": 'l'})
+            for row in right_rows + left_rows:
+                name = row.findAll(name="span", attrs={"class": "name"})[0].text.strip()
+                value = row.findAll(name="span", attrs={"class": "value"})[0].text.strip()
+                archive_dict[name] = value
+        except:
+            archive_dict = defaultdict(str)
+
+        # 5. 项目优点
+        try:
+            merit_div = box_info.findAll(name="section", attrs={"class": "fixed-list", "id": "merit"})[0]
+            merit = [s.replace("；", "").strip() for s in
+                     merit_div.findAll(name="p", attrs={"class": "p1"})[0].text.strip().split("\n")]
+        except:
+            merit = []
+
+        # 6. 项目优点
+        try:
+            defect_div = box_info.findAll(name="section", attrs={"class": "fixed-list", "id": "defect"})[0]
+            defect = [s.replace("；", "").strip() for s in
+                      defect_div.findAll(name="p", attrs={"class": "p1"})[0].text.strip().split("\n")]
+        except:
+            defect = []
+
+        processed_results.append(
+            [
+                url, item_name, ','.join(item_aliases), item_desc,
+                ",".join(effects), ','.join(items),
+                archive_dict["品牌"], archive_dict["产地"], archive_dict['公司/厂商'],
+                ','.join(merit), ",".join(defect)
+            ]
+        )
+
+    max_len = max([len(ls) for ls in processed_results])
+    processed_results = [ls + [""] * (max_len - len(ls)) for ls in processed_results]
+    processed_results_df = pd.DataFrame(processed_results,
+                                        columns=['链接', '仪器/材料名', '别名', '简介', '功效', '适用项目',
+                                                 '品牌', '产地', '公司/厂商', '优点', '缺点'])
+    processed_results_df.to_excel(out_path, index=False)
+
+
 if __name__ == '__main__':
     # baike_from_xinyang()
-    parsing_item_html("data/yimei", "data/yimei_processed/item.xlsx")
+    # parsing_item_html("data/yimei", "data/yimei_processed/item.xlsx")
+    parsing_device_html("data/yimei", "data/yimei_processed/device.xlsx", "device")
+    parsing_device_html("data/yimei", "data/yimei_processed/material.xlsx", "material")
